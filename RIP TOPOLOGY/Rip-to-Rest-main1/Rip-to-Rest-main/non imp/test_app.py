6a8a103439f75353081aba3d0f6c1fb561b7d53c
Askarr 230104752 <mohamed230104752@sut.edu.eg>
2025-05-21 13:45:15 +0300

    dd

--- RIP TOPOLOGY/Rip-to-Rest-main1/Rip-to-Rest-main/non imp/test_app.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-
-def test_app_creation():
-    app = create_app()
-    assert app is not None
-    print("Application created successfully!")
-
-if __name__ == '__main__':
-    test_app_creation()
